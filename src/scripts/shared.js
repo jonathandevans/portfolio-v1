703562d0b16a76a0d265a20e5385275f07b1d9ea
Jonathan E <115096710+jonathanedev@users.noreply.github.com>
2023-02-08 16:51:46 +0000

    Refactor JavaScript
    I have added comments and formatted the JavaScript for future use.

--- src/scripts/shared.js
+++ src/scripts/shared.js
@@ -1,3 +1,8 @@
+"use strict";
+
+/**
+ * Used to setup the click events for the mobile navigation.
+ */
 function mobileNavSetup() {
     const mobileNavToggle = document.getElementById("mobile-nav__toggle");
     const mobileNav = document.getElementById("mobile-nav");
@@ -6,12 +11,22 @@ function mobileNavSetup() {
 
     mobileNavToggle.addEventListener("click", toggleMobileNav);
     backdrop.addEventListener("click", toggleMobileNav);
-    for (b of buttons) b.addEventListener("click", toggleMobileNav);
+    for (let b of buttons) b.addEventListener("click", toggleMobileNav);
 
+    /**
+     * Used to toggle the classes to show/hide the mobile navigation.
+     */
     function toggleMobileNav() {
         mobileNav.classList.toggle("mobile-nav--open");
         backdrop.classList.toggle("backdrop--open");
     }
 }
 
-mobileNavSetup();
+/**
+ * Used to add javascript functionality to the page when loaded.
+ */
+function generatePage() {
+    mobileNavSetup();
+}
+
+document.body.onload = generatePage();
